521d1f2a10501e1363e72913d79a975ac44a4124
Valquiria <valtavares91@hotmail.com>
2024-05-21 11:59:23 -0300

    conta terminal

---
+++ ContaBanco/src/Scanner.java
@@ -0,0 +1,14 @@
+
+public class Scanner {
+
+    public String next() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+
+    public double nextDouble() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextDouble'");
+    }
+
+}
